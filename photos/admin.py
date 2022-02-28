b4f77752c548730a276c820015d1a036312d09d9
SubiraMbula <subira.savali@student.moringaschool.com>
2022-02-28 07:41:07 +0300

    changes on models files

--- photos/admin.py
+++ photos/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Category,Image,Location
+
+admin.site.register(Category),
+admin.site.register(Image),
+admin.site.register(Location)
