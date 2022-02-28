ec4c68487acc7f6040e8bdd0428cf1663e16a1d7
SubiraMbula <subira.savali@student.moringaschool.com>
2022-02-28 08:14:25 +0300

    Final Deployment to heroku

--- photos/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Category,Image,Location
-
-admin.site.register(Category),
-admin.site.register(Image),
-admin.site.register(Location)
